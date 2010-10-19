b8fbbdc2fb7c8d092027ef188ab5f3290f5b494c
Nicolas Brodu <nicolas.brodu@numerimoire.net>
2010-10-19 23:47:49 +0200

    SVM would be fine but both too complicated (devaluate the importance of having good features) and too slow to train. Showing that good classification is OK with the raw features and least-squares hyperplanes have much more impact!

--- make_features.cpp
+++ make_features.cpp
@@ -21,7 +21,6 @@ make_features features.prm data1.msc data2.msc - data3.msc - data4.msc...\n\
         return 0;
 }
 
-
 int main(int argc, char** argv) {
     
     if (argc<4) return help();
@@ -36,6 +35,7 @@ int main(int argc, char** argv) {
     
     ofstream classifparamsfile(argv[1], ofstream::binary);
 
+    // read headers and ensures all files are consistent
     for (int argi = 2; argi<argc; ++argi) {
         if (!strcmp("-",argv[argi])) {
             ++nclasses;
@@ -63,7 +63,6 @@ int main(int argc, char** argv) {
             for (int si=0; si<nscales; ++si) if (scales[si]!=scales_thisfile[si]) {cerr<<"input file mismatch: "<<argv[argi]<<endl; return 1;}
         }
         mscfile.close();
-        // this allows to pre-allocate the big matrix for the svd
         total_pts += npts;
     }
     classboundaries.push_back(total_pts);
@@ -73,13 +72,14 @@ int main(int argc, char** argv) {
         return 1;
     }
     
-    // allocate the big matrix. it will be column-major,
+    // number of features
     int fdim = nscales * 2;
-    vector<FloatType> A(total_pts * fdim);
-    vector<FloatType> avg_feat(fdim, 0);
-    
+
+    // store all data in memory, keep overhead to a minimum as we plan for a lot of points...
+    vector<FloatType> data(total_pts * fdim);
+
     int base_pt = 0;
-    // and then fill it with the files
+    // and then fill data from the files
     for (int argi = 2; argi<argc; ++argi) {
         if (!strcmp("-",argv[argi])) continue;
         ifstream mscfile(argv[argi], ifstream::binary);
@@ -92,174 +92,93 @@ int main(int argc, char** argv) {
         vector<FloatType> scales_thisfile(nscales_thisfile);
         for (int si=0; si<nscales_thisfile; ++si) mscfile.read((char*)&scales_thisfile[si], sizeof(FloatType));
         
-        // now fill in the matrix with the points
+        // now fill in the big data storage with the points
         for (int pt=0; pt<npts; ++pt) {
             int ptidx; // we do not care for the point order here
             mscfile.read((char*)&ptidx, sizeof(ptidx));
-            
-/*
-            for (int f=0; f<fdim; ++f) {
-                // matrix is column-major, but input file is row-major...
-                FloatType value;
-                mscfile.read((char*)(&value), sizeof(FloatType));
-                A[f*total_pts+base_pt+pt] = value;
-                avg_feat[f] += value;
-            }
-*/
-            for (int f=0; f<nscales; ++f) {
-                // matrix is column-major, but input file is row-major...
+            for (int s=0; s<nscales; ++s) {
                 FloatType a,b;
                 mscfile.read((char*)(&a), sizeof(FloatType));
                 mscfile.read((char*)(&b), sizeof(FloatType));
                 FloatType c = 1 - a - b;
                 // project in the equilateral triangle a*(0,0) + b*(1,0) + c*(1/2,sqrt3/2)
-                // so each dimension is given equal weight during the PCA
-                // is this necessary ?
+                // equivalently to the triangle formed by the three components unit vector
+                // (1,0,0), (0,1,0) and (0,0,1) when considering a,b,c in 3D
+                // so each a,b,c = dimensionality of the data is given equal weight
+                // is this necessary ? => not for linear classifiers, but plan ahead...
                 FloatType x = b + c / 2;
-                FloatType y = c * sqrt(3)/2;                
-                A[(f*2)*total_pts+base_pt+pt] = x;
-                avg_feat[f*2] += x;
-                A[(f*2+1)*total_pts+base_pt+pt] = y;
-                avg_feat[f*2+1] += y;
+                FloatType y = c * sqrt(3)/2;
+                data[(base_pt+pt)*fdim + s*2] = x;
+                data[(base_pt+pt)*fdim + s*2+1] = y;
             }
         }
         mscfile.close();
         base_pt += npts;
     }
+
+    // Now the one-against-one fitting of hyperplanes
+    // there will be (nclasses * (nclasses-1) / 2) classifiers
+    // let i,j and i<j be two classes to compare (hence j>=1 and i>=0)
+    // the classifier for these two classes is stored idx = j*(j-1)/2 + i
+    // each classifier is a set of fdim+1 weights = one hyperplane that may be shifted from origin
+    vector<vector<FloatType> > weights(nclasses * (nclasses-1) / 2, vector<FloatType>(fdim+1));
     
-    // now center the matrix to properly do the PCA on the covariance matrix using SVD
-    for (int f=0; f<fdim; ++f) {
-        avg_feat[f] /= total_pts;
-        for (int pt=0; pt<total_pts; ++pt) A[f*total_pts+pt] -= avg_feat[f];
-    }
-    
-    // singular values
-    vector<FloatType> S(fdim);
-    // principal component vector basis
-    vector<FloatType> B(fdim*fdim);
-
-    // make a copy, svd destroys the original matrix
-    vector<FloatType> Aori = A;
-    
-    // now do the big svd, keeping all projected series in A for further classification
-    svd(total_pts, fdim, &A[0], &S[0], true, &B[0]);
-
-/*    // display first observation
-    cout << "s=[ "; for (int comp=0; comp<fdim; ++comp) cout << S[comp] << " "; cout << "]"<<endl;
-    cout << "aori=[ "; for (int comp=0; comp<fdim; ++comp) cout << Aori[comp*total_pts] << " "; cout << "]"<<endl;
-    cout << "acmp=[ "; for (int comp=0; comp<fdim; ++comp) cout << A[comp*total_pts] << " "; cout << "]"<<endl;
-    cout << "b=["; for (int comp=0; comp<fdim; ++comp) {
-        for (int f=0; f<fdim; ++f) cout << B[f*fdim+comp] << " ";
-        if (comp<fdim-1) cout << "; "; else cout << "]";
-    } cout << endl;
-*/  
-
-    // sanity check: project the original data on the basis vectors, shall find the svd series
-    // first scale the B matrix by the singular values so as to get the projected series directly
-    for (int comp=0; comp<fdim; ++comp) {
-        for (int f=0; f<fdim; ++f) B[f*fdim+comp] /= S[comp];
-    }
-    vector<FloatType> avgdiff(fdim,0), avgscale(fdim,0);
-    for (int obs = 0; obs<total_pts; ++obs) {
-        // for each component
-        for (int comp=0; comp<fdim; ++comp) {
-            double res = 0;
-            // that component is the observation dot the unit vector, times S[comp]
-            for (int f=0; f<fdim; ++f) res += Aori[f*total_pts + obs] * B[f*fdim+comp];
-            //cout << res << " shall be equal to " << A[comp * total_pts + obs] << endl;
-            avgdiff[comp] += fabs(A[comp * total_pts + obs] - res);
-            avgscale[comp] += fabs(A[comp * total_pts + obs] + res) * 0.5;
-        }
-    }
-    cout << "Reliability of each component : average difference between calculated series and projected one, compared to data scale" << endl;
-    int nreliablecomp = 0, hasunreliable = 0;
-    for (int comp=0; comp<fdim; ++comp) {
-        cout << (avgdiff[comp]/total_pts) << " / " << (avgscale[comp]/total_pts);
-        if (avgdiff[comp]/avgscale[comp] < 1e-6) {
-            if (hasunreliable) cout << " would be OK except for previous components" << endl;
-            else {
-                cout << " OK (<1e-6)" << endl;
-                nreliablecomp++;
-            }
-        }
-        else cout << " unreliable" << endl;
-    }
-    cout << endl;
-
-    // Output file contains all valid scaled projection vectors and classif params
-    classifparamsfile.write((char*)&nreliablecomp, sizeof(int));
-    for (int comp=0; comp<nreliablecomp; ++comp) {
-        // write the B vector for this component, row-major to simplify later reuse...
-        for (int f=0; f<fdim; ++f) B[f*fdim+comp];
-        
-        // TODO: here
-        
-    }
-
-    
-    // informative output, may help select a number of features
-    cout << "Total variance explained by each reliable feature" << endl;
-    FloatType totalvar = 0;
-    for (int f=0; f<fdim; ++f) totalvar += S[f] * S[f];
-    for (int f=0; f<nreliablecomp; ++f) {
-        cout << " " << (S[f] * S[f] / totalvar) << endl;
-    }
-    cout << endl;
-    cout << "Cumulated variance explained by up to each feature" << endl;
-    FloatType cumvar = 0;
-    for (int f=0; f<nreliablecomp; ++f) {
-        cumvar += S[f] * S[f];
-        cout << " " << (cumvar / totalvar) << endl;
-    }
-    cout << endl;
-
-    // set of weights for classifying each class, at each level of retained components
-    vector<vector<FloatType> > weights[nclasses];
-    
-    // std technique : ssq to separate one class from all others => 1 set of weights for each class
-    // apply that to each subset of features
-    for (int f=0; f<nreliablecomp; ++f) {
-        int ncomp = f+1;
-        FloatType accuracy = 0;
-        for (int c = 0; c < nclasses; ++c) {
-            // ncomp+1 coefficients for the hyperplane def (aka col of 1, shifting coef, etc)
-            weights[c].push_back(vector<FloatType>(ncomp+1,0));
+    // train all one-against-one classifiers
+    for (int jclass = 1; jclass < nclasses; ++jclass) {
+        int jbeg = classboundaries[jclass];
+        int jend = classboundaries[jclass+1];
+        int nj = jend - jbeg;
+        for (int iclass = 0; iclass < jclass; ++iclass) {
+            int ibeg = classboundaries[iclass];
+            int iend = classboundaries[iclass+1];
+            int ni = iend - ibeg;
+            int ntotal = ni + nj;
             // need to allocate a new matrix as the content is destroyed by the algorithm
-            // Add a column of 1 to the projected information so that we can properly do the least-square fitting
-            vector<FloatType> Asel(total_pts * (ncomp+1));
-            // Copy the matrix, column-major = just take the first portion of memory
-            memcpy(&Asel[0], &A[0], ncomp * total_pts * sizeof(FloatType));
-            // Now add the column of 1
-            for (int pt=0; pt<total_pts; ++pt) Asel[ncomp * total_pts + pt] = 1;
-            // Prepare B = the classes indicator. within class = +1, outside = -1
-            vector<FloatType> Bsel(total_pts);
-            int beg = classboundaries[c];
-            int end = classboundaries[c+1]; // was filled with total_pts at nclasses
-            for (int pt=0; pt<beg; ++pt) Bsel[pt] = -1;
-            for (int pt=beg; pt<end; ++pt) Bsel[pt] = 1;
-            for (int pt=end; pt<total_pts; ++pt) Bsel[pt] = -1;
-            // Do the least square fit
-            leastSquares(&Asel[0], total_pts, ncomp+1, &Bsel[0], 1);
-            // result is the ncomp+1 first entries of B
-            for (int i=0; i<=ncomp; ++i) weights[c].back()[i] = Bsel[i];
-            // compute the classification error for this class
-            int ncorrectclasses = 0;
-            for (int pt=beg; pt<end; ++pt) {
+            // Add a column of 1 so as to allow hyperplanes not necessarily going through the origin
+            // A is column-major... and each row of A is an instance
+            vector<FloatType> A(ntotal * (fdim+1));
+            // Prepare B = the classes indicator. class i = -1, j = +1
+            vector<FloatType> B(ntotal);
+            // fill the matrices with the classes data
+            for (int pt=0; pt<ni; ++pt) {
+                for(int f=0; f<fdim; ++f) A[f * ntotal + pt] = data[(ibeg+pt)*fdim + f];
+                A[fdim*ntotal + pt] = 1;
+                B[pt] = -1;
+            }
+            for (int pt=0; pt<nj; ++pt) {
+                for(int f=0; f<fdim; ++f) A[f * ntotal + (pt+ni)] = data[(jbeg+pt)*fdim + f];
+                A[fdim*ntotal + (pt+ni)] = 1;
+                B[pt+ni] = 1;
+            }
+            // now the least squares hyperplane fit
+            leastSquares(&A[0], ntotal, fdim+1, &B[0], 1);
+            // result is the fdim+1 first entries of B
+            // compute the classification error
+            int ncorrecti = 0;
+            for (int pt=0; pt<ni; ++pt) {
                 FloatType pred = 0;
-                for (int i=0; i<ncomp; ++i) pred += A[i * total_pts + pt] * Bsel[i];
-                pred += Bsel[ncomp]; // factor for the column of 1
-                if (pred>0) ++ncorrectclasses;
+                for(int f=0; f<fdim; ++f) pred += data[(ibeg+pt)*fdim + f] * B[f];
+                pred += B[fdim]; // bias at origin
+                if (pred<0) ++ncorrecti;
+            }
+            int ncorrectj = 0;
+            for (int pt=0; pt<nj; ++pt) {
+                FloatType pred = 0;
+                for(int f=0; f<fdim; ++f) pred += data[(jbeg+pt)*fdim + f] * B[f];
+                pred += B[fdim]; // bias at origin
+                if (pred>=0) ++ncorrectj;
             }
             // accuracy that gives equal weight to each class
-            accuracy += (FloatType)ncorrectclasses / (FloatType)(end-beg);
+            FloatType accuracy = 0.5 * (ncorrecti / (FloatType)ni + ncorrectj / (FloatType)nj);
+            // that accuracy will be the voting weight for this classifer when predicting new points
+            cout << "accuracy for discriminating classes " << iclass << " and " << jclass << " is: " << accuracy << endl;
+            cout << "set of weights + bias:";
+            for(int f=0; f<fdim; ++f) cout << " " << B[f];
+            cout << " " << B[fdim] << endl;
         }
-        accuracy /= nclasses;
-        cout << "Classification accuracy for " << (f+1) << " retained component(s): " << (accuracy*100) << "%" << endl;
-        //if (accuracy==1) break;
     }
-
-    // TODO: write matrix B for projection
-    // trick : scale B, not A, so that the projections of unknown points from the full data set are compatible
+    
+    // TODO: write weights in param file
         
     return 0;
 }
