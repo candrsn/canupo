e010b4e15ec3da366c92cd607af607b7765223d3
Nicolas Brodu <nicolas.brodu@numerimoire.net>
2011-05-04 11:17:58 +0200

    Added the balanced accuracy

--- validate_classifier.cpp
+++ validate_classifier.cpp
@@ -378,12 +378,13 @@ int main(int argc, char** argv) {
     // http://en.wikipedia.org/wiki/Matthews_correlation_coefficient
     cout << "True/False Positive/Negative counts: TP=" << TP << ", FN=" << FN << ", TN=" << TN << ", FP=" << FP << endl;
     cout << "Class 1 count (=TP+FN): " << ndata_class1 << ", " << "Class 2 count (=TN+FP): " << ndata_class2 << endl;
-    cout << "Accuracy: " << FloatType(TP+TN) / FloatType(nsamples) << endl;
     cout << "Sensitivity (true positive rate): " << FloatType(TP) / FloatType(ndata_class1) << endl;
     cout << "Specificity (true negative rate): " << FloatType(TN) / FloatType(ndata_class2) << endl;
     FloatType mcc = FloatType(TP+FP)*FloatType(TP+FN)*FloatType(TN+FP)*FloatType(TN+FN);
-    if (mcc<=0) mcc = 1;
+    if (mcc<=0) mcc = 0;
     else mcc = (FloatType(TP*TN) - FloatType(FP*FN)) / sqrt(mcc);
-    cout << "Matthews correlation coefficient: " << mcc << endl;
+    cout << "Matthews correlation coefficient (note: -1 ≤ mcc ≤ 1): " << mcc << endl;
+    cout << "Accuracy: " << FloatType(TP+TN) / FloatType(nsamples) << endl;
+    cout << "Balanced Accuracy: " << 0.5 * (FloatType(TP) / FloatType(ndata_class1) + FloatType(TN) / FloatType(ndata_class2)) << endl;
     return 0;
 }
