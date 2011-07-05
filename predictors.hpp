4ad5874e2f811d3f4a6c9d9d87e31612d6d31e19
Nicolas Brodu <nicolas.brodu@numerimoire.net>
2011-07-05 21:16:27 +0200

    Added licencing information (LGPL v2+)

--- predictors.hpp
+++ predictors.hpp
@@ -1,3 +1,26 @@
+//**********************************************************************
+//* This file is a part of the CANUPO project, a set of programs for   *
+//* classifying automatically 3D point clouds according to the local   *
+//* multi-scale dimensionality at each point.                          *
+//*                                                                    *
+//* Author & Copyright: Nicolas Brodu <nicolas.brodu@numerimoire.net>  *
+//*                                                                    *
+//* This project is free software; you can redistribute it and/or      *
+//* modify it under the terms of the GNU Lesser General Public         *
+//* License as published by the Free Software Foundation; either       *
+//* version 2.1 of the License, or (at your option) any later version. *
+//*                                                                    *
+//* This library is distributed in the hope that it will be useful,    *
+//* but WITHOUT ANY WARRANTY; without even the implied warranty of     *
+//* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU  *
+//* Lesser General Public License for more details.                    *
+//*                                                                    *
+//* You should have received a copy of the GNU Lesser General Public   *
+//* License along with this library; if not, write to the Free         *
+//* Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston,    *
+//* MA  02110-1301  USA                                                *
+//*                                                                    *
+//**********************************************************************/
 #ifndef CANUPO_PREDICTORS_HPP
 #define CANUPO_PREDICTORS_HPP
 
